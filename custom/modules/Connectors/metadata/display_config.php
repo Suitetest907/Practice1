a0cc079d5adcd5d13c7da232017fac6bf7c37e63
Karamat Ali <karamat.ali@RT-LT-397.rolustech.net>
2020-03-06 11:25:34 +0500

    first commit

---
+++ custom/modules/Connectors/metadata/display_config.php
@@ -0,0 +1,23 @@
+<?php
+// created: 2020-03-03 16:29:50
+$modules_sources = array (
+  'Accounts' => 
+  array (
+    'ext_rest_twitter' => 'ext_rest_twitter',
+  ),
+  'Contacts' => 
+  array (
+    'ext_rest_twitter' => 'ext_rest_twitter',
+  ),
+  'Leads' => 
+  array (
+    'ext_rest_twitter' => 'ext_rest_twitter',
+  ),
+  'Prospects' => 
+  array (
+    'ext_rest_twitter' => 'ext_rest_twitter',
+  ),
+  'Opportunities' => 
+  array (
+  ),
+);
